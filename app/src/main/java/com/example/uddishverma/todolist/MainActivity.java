753a5495d13b5c5eac259eac274fc8147ad3a45c
Uddish Verma <uddishverma22@gmail.com>
2016-07-20 18:51:23 +0530

    Delete Working Properly

--- app/src/main/java/com/example/uddishverma/todolist/MainActivity.java
+++ app/src/main/java/com/example/uddishverma/todolist/MainActivity.java
@@ -11,8 +11,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -43,89 +45,93 @@ public class MainActivity extends AppCompatActivity {
         db = new TaskDbHelper(this);
         list = db.getAllTasks();
         btnRemove = (Button) findViewById(R.id.btn_remove);
-        adapt = new MyAdapter(this, R.layout.list_layout, list);
+        adapt = new MyAdapter(list);
         listView = (ListView) findViewById(R.id.list_main);
         listView.setAdapter(adapt);
+
+        btnRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db.deleteTask();
+                list = db.getAllTasks();
+                Log.d(TAG, "onClick: " + list.size());
+                adapt.notifyDataSetChanged();
+            }
+        });
     }
+
     //OnCLick for the button
-    public void addTaskNow(View v)  {
+    //TODO add this task in the database not here
+    public void addTaskNow(View v) {
         et_add = (EditText) findViewById(R.id.et_task);
         String s = et_add.getText().toString();
         Task task = new Task(s, 0);
+
         db.addTask(task);
-        et_add.setText("");
-        adapt.add(task);
+        list = db.getAllTasks();
+
         adapt.notifyDataSetChanged();
 
+        et_add.setText("");
     }
 
 
-    public class MyAdapter extends ArrayAdapter<Task>   {
+    public class MyAdapter extends BaseAdapter {
 
-        Context context;
-        ArrayList<Task> taskList = new ArrayList<>();
-        int layoutResourceId;
+        ArrayList<Task> taskList = null;
 
-        public MyAdapter(Context context, int layoutResourceId,
-                         List<Task> objects) {
-            super(context, layoutResourceId, objects);
-            this.layoutResourceId = layoutResourceId;
+
+        public MyAdapter(List<Task> objects) {
             this.taskList = (ArrayList<Task>) objects;
-            this.context = context;
         }
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
+        public int getCount() {
+            return taskList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return taskList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
             CheckBox chk = null;
             LayoutInflater li = getLayoutInflater();
-            if(convertView == null) {
+            if (convertView == null) {
                 convertView = li.inflate(R.layout.list_layout, null);
                 chk = (CheckBox) convertView.findViewById(R.id.checkBox);
                 convertView.setTag(chk);
-
-                if (chk != null) {
-                    chk.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-//                           In other words, it asserts to the compiler that v has type CheckBox
-//                           and thus it is OK to call the methods of CheckBox on v.
-                            CheckBox cb = (CheckBox) v;
-                            final Task checkTask = (Task) cb.getTag();
-                            if(!cb.isChecked()) {
-                                checkTask.setStatus(0);
-                                cb.setTextColor(Color.RED);
-                                Toast.makeText(getApplicationContext(), "update function " + cb.getText() + " " +  cb.isChecked(), Toast.LENGTH_SHORT).show();
-                                db.updateTask(checkTask);
-                            }
-                            else    {
-                                checkTask.setStatus(1);
-                                cb.setTextColor(Color.WHITE);
-                                    btnRemove.setOnClickListener(new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View v) {
-                                            if(checkTask.getStatus() == 1) {
-                                                db.deleteTask(checkTask);
-                                                adapt.remove(checkTask);
-                                                adapt.notifyDataSetChanged();
-                                            }
-                                        }
-                                    });
-                                //To strike through the text
-//                                cb.setPaintFlags(cb.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
-                                Toast.makeText(getApplicationContext(), "update function " + cb.getText() + " " + cb.isChecked(), Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
-                }
-            }
-            else    {
+            } else {
                 chk = (CheckBox) convertView.getTag();
             }
+            chk.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    final Task checkTask = taskList.get(position);
+                    if (isChecked) {
+                        checkTask.setStatus(1);
+                        buttonView.setTextColor(Color.WHITE);
+                        db.updateTask(checkTask);
+                    } else {
+                        checkTask.setStatus(0);
+                        buttonView.setTextColor(Color.RED);
+                        Toast.makeText(getApplicationContext(), "update function " + buttonView.getText() + " " + isChecked, Toast.LENGTH_SHORT).show();
+                        db.updateTask(checkTask);
+                    }
+                }
+            });
             Task current = taskList.get(position);
             Log.d(TAG, "getView: " + current.getTaskName());
 //            tvDate.setText(current.getDate());
             chk.setText(current.getTaskName());
-            chk.setChecked((current.getStatus()==1) ? true : false);
+            chk.setChecked((current.getStatus() == 1) ? true : false);
             chk.setTag(current);
             return convertView;
         }
